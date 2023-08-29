6cb0cd609309d1c338576fca6ee1473b0de6f0d9
arrlancores <arrlancore@gmail.com>
2023-08-29 09:25:21 +0700

    feat: error boundary

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppContainer from "components/app-container";
 import theme from "theme";
+import ErrorBoundary from "components/error-boundary";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyles />
-      <AppContainer theme={theme}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Router>
-      </AppContainer>
+      <ErrorBoundary>
+        <GlobalStyles />
+        <AppContainer theme={theme}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Router>
+        </AppContainer>
+      </ErrorBoundary>
     </div>
   );
 }
