6d3c4cde1cc9e3f68a6670c76c4912b2c200b562
arrlancores <arrlancore@gmail.com>
2023-08-29 11:55:15 +0700

    feat: add card user

--- src/features/home/page.js
+++ src/features/home/page.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { styled } from "styled-components";
 import { searchGithub } from "./search-slice";
+import PropTypes from "prop-types";
 
 const BodyContainer = styled.div`
   padding: 16px;
@@ -45,31 +46,105 @@ const GridContent = styled.div`
 `;
 
 const GridItem = styled.div`
-  border: 1px solid #eeeeee;
   padding: 8px;
 `;
+
+// user card component
+const Card = styled.div`
+  border: 1px solid #e1e4e8;
+  border-radius: 6px;
+  padding: 16px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ProfileImage = styled.img`
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
+  margin-bottom: 12px;
+`;
+
+const NameLink = styled.a`
+  font-size: 18px;
+  font-weight: bold;
+  color: #0366d6;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Location = styled.p`
+  font-size: 14px;
+  color: #586069;
+  margin-top: 8px;
+`;
+
+const UserCard = (props) => {
+  return (
+    <Card>
+      <ProfileImage src={props.avatarUrl} alt={props.login} />
+      <NameLink href={props.htmlUrl} target="_blank" rel="noopener noreferrer">
+        {props.name || props.login}
+      </NameLink>
+      {props.location && <Location>{props.location}</Location>}
+    </Card>
+  );
+};
+
+UserCard.propTypes = {
+  avatar_url: PropTypes.string.isRequired,
+  html_url: PropTypes.string.isRequired,
+  login: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  location: PropTypes.string,
+};
+
+const Item = ({ type, data }) => {
+  switch (type) {
+    case "users":
+      return (
+        <UserCard
+          avatarUrl={data.avatar_url}
+          htmlUrl={data.html_url}
+          login={data.login}
+          name={data.name}
+          location={data.location}
+        />
+      );
+
+    case "repository":
+      return null;
+
+    default:
+      return null;
+  }
+};
 
 const Home = () => {
   const defaultType = "users";
   const [query, setQuery] = useState("");
-  const [type, setType] = useState(defaultType); // or 'users'
+  const [type, setType] = useState(defaultType);
   const [page, setPage] = useState(1);
   const dispatch = useDispatch();
   const data = useSelector((state) => state.search.data[query]);
 
   const handleInputChange = (e) => {
     setQuery(e.target.value);
-    setPage(1); // Reset to the first page when the query changes
+    setPage(1); // Reset page
     dispatch(searchGithub({ query: e.target.value, type, page }));
   };
 
   const handleTypeChange = (e) => {
     setType(e.target.value);
-    setPage(1); // Reset to the first page when the type changes
+    setPage(1); // Reset page
     dispatch(searchGithub({ query, type: e.target.value, page }));
   };
 
-  console.log(data);
   return (
     <BodyContainer>
       <SearchContainer>
@@ -90,9 +165,11 @@ const Home = () => {
         />
       </SearchContainer>
       <GridContent>
-        <GridItem>search 1</GridItem>
-        <GridItem>search 2</GridItem>
-        <GridItem>search 3</GridItem>
+        {data?.items?.map((item) => (
+          <GridItem key={item.id}>
+            <Item type={type} data={item} />
+          </GridItem>
+        ))}
       </GridContent>
     </BodyContainer>
   );
